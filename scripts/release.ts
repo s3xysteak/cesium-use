a0fb46136b0378a8ef1e60c017178706d3c2c3c3
s3xysteak <j.yulia@qq.com>
2024-03-09 11:50:48 +0800

    ci: release script.

---
+++ scripts/release.ts
@@ -0,0 +1,17 @@
+import { consola } from 'consola'
+import { execSync } from 'node:child_process'
+
+type Version = 'major' | 'minor' | 'patch'
+const isValidVersion = (version: string) => {
+  return ['major', 'minor', 'patch'].includes(version)
+}
+
+const version = process.argv[2] as Version
+if (!isValidVersion(version)) {
+  consola.error(new Error('Invalid version'))
+  process.exit(1)
+}
+
+execSync(`npm version ${version} -m "release: %s"`, { stdio: 'inherit' })
+execSync('git push --tags', { stdio: 'inherit' })
+execSync('git push', { stdio: 'inherit' })
