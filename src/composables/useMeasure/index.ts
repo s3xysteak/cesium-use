2821452307fe0a7ff3ad30441a3a954d0117aebd
s3xysteak <j.yulia@qq.com>
2024-05-09 11:27:19 +0800

    fix(composables): type fix . `useMeasure` could receive a undefined `options` param.

--- src/composables/useMeasure/index.ts
+++ src/composables/useMeasure/index.ts
@@ -6,10 +6,10 @@ type UseMeasureType = 'area' | 'distance' | 'height'
 type UseMeasureOptions = AreaOptions | DistanceOptions | HeightOptions
 type UseMeasureReturns = AreaReturn | DistanceReturn | HeightReturn
 
-export function useMeasure(type: 'area', options: AreaOptions): AreaReturn
-export function useMeasure(type: 'distance', options: DistanceOptions): DistanceReturn
-export function useMeasure(type: 'height', options: HeightOptions): HeightReturn
-export function useMeasure(type: UseMeasureType, options: UseMeasureOptions): UseMeasureReturns {
+export function useMeasure(type: 'area', options?: AreaOptions): AreaReturn
+export function useMeasure(type: 'distance', options?: DistanceOptions): DistanceReturn
+export function useMeasure(type: 'height', options?: HeightOptions): HeightReturn
+export function useMeasure(type: UseMeasureType, options?: UseMeasureOptions): UseMeasureReturns {
   const typeMap = {
     area,
     distance,
