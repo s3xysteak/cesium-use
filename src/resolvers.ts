08034385746caa277ba59787b1a189331f4a1988
s3xysteak <86149525+s3xysteak@users.noreply.github.com>
2024-07-31 21:29:54 +0800

    fix: resolvers type issue fixed. add devDeps unplugin-vue-components

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,6 +1,8 @@
+import type {  ComponentResolver } from 'unplugin-vue-components';
+
 const list = ['Located']
 
-export default function resolvers() {
+export default function resolvers():ComponentResolver {
   const set = new Set(list)
 
   return {
